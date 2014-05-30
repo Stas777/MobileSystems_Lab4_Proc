b1d8553283a6ee69be9c13d0fdb7ea6759bcfe5e
SDI <sdi@ubuntu.(none)>
2014-05-30 05:46:37 -0700

    changed test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ sudo rmmod calc
 sudo insmod calc.ko
 
 
-echo "3" > /proc/calc/arg1
-echo "7" > /proc/calc/arg2
-echo "+" > /proc/calc/operation
+echo "10" > /proc/calc/arg1
+echo "2 > /proc/calc/arg2
+echo "-" > /proc/calc/operation
 
 cat /proc/calc/result
